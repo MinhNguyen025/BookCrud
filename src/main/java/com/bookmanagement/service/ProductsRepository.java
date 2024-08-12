cc49c527b40da9e809319fdddd69c6938b77b328
MinhWuyn <nguyenduyminh0205@gmail.com>
2024-08-12 08:55:57 +0700

    add search function

--- src/main/java/com/bookmanagement/service/ProductsRepository.java
+++ src/main/java/com/bookmanagement/service/ProductsRepository.java
@@ -2,6 +2,12 @@ package com.bookmanagement.service;
 
 import com.bookmanagement.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductsRepository extends JpaRepository<Product, Integer> {
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword% OR p.brand LIKE %:keyword% OR p.category LIKE %:keyword%")
+    List<Product> searchProducts(@Param("keyword") String keyword);
 }
